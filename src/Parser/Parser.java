4c545e47016ca720c9562eb5022b1902a1d1e35b
Lucas Laurent <lucas.laurent@telecomnancy.eu>
2023-11-13 01:31:44 +0100

    FIX: Parser more rules

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -12,10 +12,13 @@ public class Parser {
     private static Parser instance;
     private final ErrorService errorService;
     Lexer lexer;
+    private Token currentToken;
 
     private Parser() {
         this.lexer = Lexer.getInstance();
         this.errorService = ErrorService.getInstance();
+        this.currentToken = lexer.nextToken();
+        fichier();
     }
 
     public static Parser getInstance() {
@@ -37,25 +40,21 @@ public class Parser {
         analyseTerminal(Tag.PROCEDURE);
         analyseTerminal(Tag.IDENT);
         analyseTerminal(Tag.IS);
+        decls();
+        analyseTerminal(Tag.BEGIN);
+        instrs();
+        analyseTerminal(Tag.END);
+        hasident();
+        analyseTerminal(Tag.SEMICOLON);
+        analyseTerminal(Tag.EOF);
 
-    }
-
-    private void decls() {
-
-        switch (lexer.nextToken().tag()) {
-            case Tag.PROCEDURE, Tag.IDENT, Tag.TYPE, Tag.FUNCTION -> {
-                decl();
-                decls();
-            }
-            case Tag.BEGIN -> {
-            }
-        }
     }
 
     private void decl() {
 
-        switch (lexer.nextToken().tag()) {
-            case Tag.PROCEDURE -> {
+        switch (currentToken.tag()) {
+            case PROCEDURE -> {
+                analyseTerminal(Tag.PROCEDURE);
                 analyseTerminal(Tag.IDENT);
                 hasparams();
                 analyseTerminal(Tag.IS);
@@ -66,19 +65,21 @@ public class Parser {
                 hasident();
                 analyseTerminal(Tag.SEMICOLON);
             }
-            case Tag.IDENT -> {
+            case IDENT -> {
                 identsep();
                 analyseTerminal(Tag.COLON);
                 type_n();
                 typexpr();
                 analyseTerminal(Tag.SEMICOLON);
             }
-            case Tag.TYPE -> {
+            case TYPE -> {
+                analyseTerminal(Tag.TYPE);
                 analyseTerminal(Tag.IDENT);
                 hasischoose();
                 analyseTerminal(Tag.SEMICOLON);
             }
-            case Tag.FUNCTION -> {
+            case FUNCTION -> {
+                analyseTerminal(Tag.FUNCTION);
                 analyseTerminal(Tag.IDENT);
                 hasparams();
                 analyseTerminal(Tag.RETURN);
@@ -97,92 +98,280 @@ public class Parser {
 
     private void hasischoose() {
 
-        switch (lexer.nextToken().tag()) {
-            case Tag.IS -> {
+        switch (currentToken.tag()) {
+            case IS -> {
+                analyseTerminal(Tag.IS);
                 accorrec();
             }
-            case Tag.SEMICOLON -> {
+            case SEMICOLON -> {
             }
         }
     }
 
-    private void hasparams() {
+    private void accorrec() {
 
-        switch (lexer.nextToken().tag()) {
-            case Tag.IS -> {
+        switch (currentToken.tag()) {
+            case ACCESS -> {
+                analyseTerminal(Tag.ACCESS);
+                analyseTerminal(Tag.IDENT);
             }
-            case Tag.RETURN -> {
+            case RECORD -> {
+                analyseTerminal(Tag.RECORD);
+                champs();
+                analyseTerminal(Tag.END);
+                analyseTerminal(Tag.RECORD);
             }
-            case Tag.OPEN_PAREN -> {
-                params();
+        }
+    }
+
+    private void decls() {
+
+        switch (currentToken.tag()) {
+            case PROCEDURE, IDENT, TYPE, FUNCTION -> {
+                decl();
+                decls();
             }
+            case BEGIN -> {
+            }
+        }
+    }
+
+    private void hasident() {
+
+        switch (currentToken.tag()) {
+            case SEMICOLON -> {
+            }
+            case IDENT -> analyseTerminal(Tag.IDENT);
+        }
+    }
+
+    private void identsep() {
+
+        if (currentToken.tag() == Tag.IDENT) {
+            analyseTerminal(Tag.IDENT);
+            identsep2();
+        }
+    }
+
+    private void identsep2() {
+
+        switch (currentToken.tag()) {
+            case COLON -> {
+            }
+            case COMMA -> {
+                analyseTerminal(Tag.COMMA);
+                identsep();
+            }
+        }
+    }
+
+    private void champ() {
+
+        if (currentToken.tag() == Tag.IDENT) {
+            identsep();
+            analyseTerminal(Tag.COLON);
+            type_n();
+            analyseTerminal(Tag.SEMICOLON);
+        }
+    }
+
+    private void champs() {
+
+        if (currentToken.tag() == Tag.IDENT) {
+            champ();
+            champs2();
+        }
+    }
+
+    private void champs2() {
+
+        switch (currentToken.tag()) {
+            case IDENT -> champs();
+            case END -> {
+            }
+        }
+    }
+
+    private void type_n() {
+
+        switch (currentToken.tag()) {
+            case ACCESS -> {
+                analyseTerminal(Tag.ACCESS);
+                analyseTerminal(Tag.IDENT);
+            }
+            case IDENT -> analyseTerminal(Tag.IDENT);
         }
     }
 
     private void params() {
 
-        switch (lexer.nextToken().tag()) {
-            case Tag.OPEN_PAREN -> {
-                paramsep();
-                analyseTerminal(Tag.CLOSE_PAREN);
+        if (currentToken.tag() == Tag.OPEN_PAREN) {
+            analyseTerminal(Tag.OPEN_PAREN);
+            paramsep();
+            analyseTerminal(Tag.CLOSE_PAREN);
+        }
+    }
+
+    private void hasparams() {
+
+        switch (currentToken.tag()) {
+            case IS, RETURN -> {
             }
+            case OPEN_PAREN -> params();
         }
     }
 
     private void paramsep() {
 
-        switch (lexer.nextToken().tag()) {
-            case Tag.IDENT -> {
-                param();
-                paramsep2();
+        if (currentToken.tag() == Tag.IDENT) {
+            param();
+            paramsep2();
+        }
+    }
+
+    private void paramsep2() {
+
+        switch (currentToken.tag()) {
+            case SEMICOLON -> {
+                analyseTerminal(Tag.SEMICOLON);
+                paramsep();
+            }
+            case CLOSE_PAREN -> {
+            }
+        }
+    }
+
+    private void typexpr() {
+
+        switch (currentToken.tag()) {
+            case ASSIGN -> {
+                analyseTerminal(Tag.ASSIGN);
+                expr();
+            }
+            case SEMICOLON -> {
             }
         }
     }
 
     private void param() {
 
-        switch (lexer.nextToken().tag()) {
-            case Tag.IDENT -> {
-                identsep();
-            }
+        if (currentToken.tag() == Tag.IDENT) {
+            identsep();
+            analyseTerminal(Tag.COLON);
+            mode();
+            type_n();
         }
     }
 
-    private void identsep() {
-
-        switch (lexer.nextToken().tag()) {
-            case Tag.IDENT -> {
-                identsep2();
-            }
-        }
+    private void mode() {
     }
 
-    private void identsep2() {
-
-        switch (lexer.nextToken().tag()) {
-            case Tag.COLON -> {
-            }
-        }
+    private void modeout() {
     }
 
-    private void paramsep2() {
-
-        switch (lexer.nextToken().tag()) {
-            case Tag.SEMICOLON -> {
-                paramsep();
-            }
-            case Tag.CLOSE_PAREN -> {
-            }
-        }
+    private void expr() {
     }
 
+    private void or_expr() {
+    }
+
+    private void or_expr2() {
+    }
+
+    private void or_expr3() {
+    }
+
+    private void and_expr() {
+    }
+
+    private void and_expr2() {
+    }
+
+    private void and_expr3() {
+    }
+
+    private void not_expr() {
+    }
+
+    private void not_expr2() {
+    }
+
+    private void equality_expr() {
+    }
+
+    private void equality_expr2() {
+    }
+
+    private void relational_expr() {
+    }
+
+    private void relational_expr2() {
+    }
+
+    private void additive_expr() {
+    }
+
+    private void additive_expr2() {
+    }
+
+    private void multiplicative_expr() {
+    }
+
+    private void multiplicative_expr2() {
+    }
+
+    private void unary_expr() {
+    }
+
+    private void primary() {
+    }
+
+    private void primary2() {
+    }
+
+    private void exprsep() {
+    }
+
+    private void exprsep2() {
+    }
+
+    private void hasexpr() {
+    }
+
+    private void instr() {
+    }
+
+    private void instr2() {
+    }
+
+    private void instr3() {
+    }
+
+    private void elifn() {
+    }
+
+    private void elsen() {
+    }
+
+    private void hasreverse() {
+    }
+
+    private void instrs() {
+    }
+
+    private void instrs2() {
+    }
+
+    private void acces() {
+    }
 
     private void analyseTerminal(Tag tag) {
-        Token token = lexer.nextToken();
-        if (!(token.tag() == tag)) {
-            Token expectedToken = new Token(tag, token.line(), TagHelper.getTagString(tag));
-            errorService.registerSyntaxError(new UnexpectedTokenException(expectedToken, token));
+        if (!(currentToken.tag() == tag)) {
+            Token expectedToken = new Token(tag, currentToken.line(), TagHelper.getTagString(tag));
+            errorService.registerSyntaxError(new UnexpectedTokenException(expectedToken, currentToken));
         }
+        // Contient le prochain token ou <EOF,currentLine,""> si fin de fichier
+        currentToken = lexer.nextToken();
     }
 
 }
